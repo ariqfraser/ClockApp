e91c4ba853483e42f53e81f22cd39830b1daeedc
Ariq Fraser <61669395+ariqfraser@users.noreply.github.com>
2021-05-20 22:23:17 +0100

    Updates

---
+++ precache-manifest.b63090fe288ed6882ddf301c6927325c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad68521f708ee4c98c9",
+    "url": "/ClockApp/static/css/main.9be69839.chunk.css"
+  },
+  {
+    "revision": "cad68521f708ee4c98c9",
+    "url": "/ClockApp/static/js/main.1bd8d441.chunk.js"
+  },
+  {
+    "revision": "09ee77d9549e94bd0943",
+    "url": "/ClockApp/static/js/runtime~main.7bc36078.js"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/ClockApp/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "5b8a88d8378f5ae0c1f1",
+    "url": "/ClockApp/static/js/3.49960275.chunk.js"
+  },
+  {
+    "revision": "2076ec0aaabe211cbbda7def1d5eac0c",
+    "url": "/ClockApp/index.html"
+  }
+];
